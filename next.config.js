7d9bc34fefff049968e6bce164f8c49c011e386b
Disuqi <disuqi.hijazi.7@gmail.com>
2024-01-23 18:24:34 +0000

    rolled back changes

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
-    },
-    env: {
-        SUPABASE_URL: process.env.SUPABASE_URL,
-        SUPABASE_KEY: process.env.SUPABASE_KEY,
     }
 }
 
